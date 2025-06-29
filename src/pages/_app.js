e81cd5f971274c443751be3162f7a4883d36a66d
talentmetrix <talentmetrixsp@gmail.com>
2025-06-29 20:58:20 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,9 @@
-export default function App({ Component, pageProps }) {
+import type { AppProps } from 'next/app';
+// The one and only place to import global CSS
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
